2f364df8d094eac6e8d69c12bbbb655ad65a147b
Ariel <Ariel.Salem1989@gmail.com>
2017-03-13 16:23:03 -0700

    server

--- server.js
+++ server.js
@@ -16,51 +16,50 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 3000; //sets our PORT
-
-/*-----------------------------------------------------------------------*/
-//SET ROUTES FOR API
-var router = express.Router();
-
-// we must add middleware to use for all our requests (GET, POST, etc...)
-router.use(function(req, res, next) {
-  console.log('something happened');
-  next(); //this makes sure we continue onto our next route without stopping here
-});
-
-router.get('/', function(req, res) {
-  res.json({ message: 'welcome to our API' }); //sending info as json data is the norm
-  res.send('Hello World!');
-});
-
-router.route('/items')
-  .post(function(req, res) {
-    var item = new Item();
-    item.name = req.body.name;
-    item.save(function(err) {
-      if (err) { res.send(err); }
-      res.json({ message: 'Item created!' });
-    });
-    .get(function(req, res) {
-      item.find(function(err, items) {
-        if (err) { res.send(err); }
-        res.json(items)
-      })
-    })
-  });
-//to be continued..
-
-
-// register our routes
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-/*-----------------------------------------------------------------------*/
+// /*-----------------------------------------------------------------------*/
 //START SERVER
-app.list(port, function() {
+app.listen(port, function() {
   console.log('Connected to the port: ', port);
 });
 
 module.exports = app;
 //to run server, type node server.js in the terminal command line
 
-/*-----------------------------------------------------------------------
+// /*-----------------------------------------------------------------------
+
+// /*-----------------------------------------------------------------------*/
+// //SET ROUTES FOR API
+// var router = express.Router();
+
+// // we must add middleware to use for all our requests (GET, POST, etc...)
+// router.use(function(req, res, next) {
+//   console.log('something happened');
+//   next(); //this makes sure we continue onto our next route without stopping here
+// });
+
+// router.get('/', function(req, res) {
+//   res.json({ message: 'welcome to our API' }); //sending info as json data is the norm
+//   res.send('Hello World!');
+// });
+
+// router.route('/items')
+//   .post(function(req, res) {
+//     var item = new Item();
+//     item.name = req.body.name;
+//     item.save(function(err) {
+//       if (err) { res.send(err); }
+//       res.json({ message: 'Item created!' });
+//     });
+//     .get(function(req, res) {
+//       item.find(function(err, items) {
+//         if (err) { res.send(err); }
+//         res.json(items)
+//       })
+//     })
+//   });
+// //to be continued..
+
+
+// // register our routes
+// // all of our routes will be prefixed with /api
+// app.use('/api', router);
