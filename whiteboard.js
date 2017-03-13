ed696ff22eb2d6073c472897ff7f7e4815b6b8e2
Ariel <Ariel.Salem1989@gmail.com>
2017-03-13 12:16:15 -0700

    whiteboarded ideas on structure

---
+++ whiteboard.js
@@ -0,0 +1,37 @@
+//SETUP THE EXPRESS CONNECTION
+// var express = require('express');
+// var app = express();
+// var bodyParser = require('body-parser');
+// var mongoose = require('mongoose');
+
+// have a standard URL (IP + PORT)
+// makes use of GET POST PUT DELETE OPTIONS
+//ESTABLISH CERTAIN REQ & RES
+
+// returns JSON data
+// app.get('/', function(req, res) {
+//   res.json('Hello World');
+// })
+
+// logs all requests to console
+// app.post('/list', function(req, res) {
+//   res.json('Your list has been updated');
+// })
+// need an app.js file
+// need a models DB
+// need a node_modules (created by npm to hold dependencies / packages)
+// package.json for node app and dependencies
+// server.js config our app
+
+//EVENTUALLY ESTABLISH CONNECTION TO DB AND HTML
+  //IN HTML:
+    //SIGNUP
+    //LOGIN
+    //ADD TO LIST
+    //DELETE ITEMS FROM LIST
+    //INVITE MEMBER TO LIST (EVENTUALLY)
+
+  //DB SHOULD KEEP TRACK OF LIST
+    //SHOULD GIVE ACCESS TO PEOPLE W/ TOKENS (EVENTUALLY)
+
+
